af79938d0f0fade9843f07433b91a8d6308c6e49
masafumimori <m.masafumimori@outlook.com>
2022-07-22 15:01:53 +0900

    change to typescript

--- src/utils/animation-frame.ts
+++ src/utils/animation-frame.ts
@@ -6,18 +6,18 @@ let lastTime = 0;
 
 export function getRequestAnimationFrame() {
   let { requestAnimationFrame } = window;
-  let prefix;
-  for (let i = 0; i < prefixes.length; i += 1) {
-    if (requestAnimationFrame) {
-      break;
-    }
-    prefix = prefixes[i];
-    requestAnimationFrame =
-      requestAnimationFrame || window[`${prefix}RequestAnimationFrame`];
-  }
+  // let prefix;
+  // for (let i = 0; i < prefixes.length; i += 1) {
+  //   if (requestAnimationFrame) {
+  //     break;
+  //   }
+  //   prefix = prefixes[i];
+  //   requestAnimationFrame =
+  //     requestAnimationFrame || window[`${prefix}RequestAnimationFrame`];
+  // }
   // fallback to setTimeout and clearTimeout if either request/cancel is not supported
   if (!requestAnimationFrame) {
-    requestAnimationFrame = () => callback => {
+    requestAnimationFrame = (callback) => {
       const currTime = new Date().getTime();
       const timeToCall = Math.max(0, 16 - (currTime - lastTime));
       const id = window.setTimeout(() => {
@@ -32,20 +32,20 @@ export function getRequestAnimationFrame() {
 
 export function getCancelAnimationFrame() {
   let { cancelAnimationFrame } = window;
-  let prefix;
-  for (let i = 0; i < prefixes.length; i += 1) {
-    if (cancelAnimationFrame) {
-      break;
-    }
-    prefix = prefixes[i];
-    cancelAnimationFrame =
-      cancelAnimationFrame ||
-      window[`${prefix}CancelAnimationFrame`] ||
-      window[`${prefix}CancelRequestAnimationFrame`];
-  }
+  // let prefix;
+  // for (let i = 0; i < prefixes.length; i += 1) {
+  //   if (cancelAnimationFrame) {
+  //     break;
+  //   }
+  //   prefix = prefixes[i];
+  //   cancelAnimationFrame =
+  //     cancelAnimationFrame ||
+  //     window[`${prefix}CancelAnimationFrame`] ||
+  //     window[`${prefix}CancelRequestAnimationFrame`];
+  // }
   // fallback to setTimeout and clearTimeout if either request/cancel is not supported
   if (!requestAnimationFrame || !cancelAnimationFrame) {
-    cancelAnimationFrame = id => {
+    cancelAnimationFrame = (id) => {
       window.clearTimeout(id);
     };
   }
